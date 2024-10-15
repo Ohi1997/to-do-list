dcc8b792fc1d59f751f36ed54623538081c852b3
Ohi1997 <ashrickarefin@gmail.com>
2024-10-15 20:26:42 +0600

    added css file

--- register.php
+++ register.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // Database info
 $host = "localhost";
 $db = "todo_list";
@@ -10,21 +10,34 @@ $conn = new mysqli($host, $user, $pass, $db);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection Failed: ". $conn->connect_error);
+    die("Connection Failed: " . $conn->connect_error);
 }
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Get data from the form
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+    
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <!-- Bootstrap  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/register-style.css">
 </head>
+
 <body>
-    <div class="container">
-    <h2 class="mt-5">User Registration</h2>
+    <div class="container" id="user-form">
+        <h2 class="mt-5">User Registration</h2>
         <form action="register.php" method="POST" class="mt-3">
             <div class="mb-3">
                 <label for="username" class="form-label">Username</label>
@@ -46,4 +59,5 @@ if ($conn->connect_error) {
         </form>
     </div>
 </body>
+
 </html>
