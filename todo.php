1bfc2d021b09899ad5ea96319bf354160b754387
Ohi1997 <ashrickarefin@gmail.com>
2024-10-25 12:11:27 +0600

    fixed cookie issue

--- todo.php
+++ todo.php
@@ -1,16 +1,6 @@
 <?php
 session_start();  // Start the session
 
-// Check if the user is logged in by checking the session
-if (!isset($_SESSION['id'])) {
-    // If user is not logged in, redirect to login page
-    header("Location: login.php");
-    exit();
-}
-
-// User is logged in, you can now display the todo list
-echo "Welcome, " . $_SESSION['username'];  // Example welcome message
-
 // Check if the user is already logged in through session
 if (!isset($_SESSION['id']) && isset($_COOKIE['user_id']) && isset($_COOKIE['username'])) {
     // Set session variables based on cookie values
@@ -21,6 +11,17 @@ if (!isset($_SESSION['id']) && isset($_COOKIE['user_id']) && isset($_COOKIE['use
     header("Location: todo.php");
     exit();
 }
+
+// Check if the user is logged in by checking the session
+if (!isset($_SESSION['id'])) {
+    // If user is not logged in, redirect to login page
+    header("Location: login.php");
+    exit();
+}
+
+// User is logged in, you can now display the todo list
+echo "Welcome, " . $_SESSION['username'];  // Example welcome message
+
 
 ?>
 <a href="logout.php" style="float: right;">Logout</a>
